beb9d79a3be241970c3b425c647907020ada2acb
parsa <parsa.miraghaie@gmail.com>
2025-02-03 21:23:07 +0200

    Add initial implementation of ML package with configuration and inference scripts

---
+++ packages/ml/main.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+from omegaconf import OmegaConf
+from ml.finetuning.unsloth import get_trainer_model
+from ml.experiment.mlflow.artifact_ingest_gcs import upload_artifact_to_mlflow_gcs
+from configparser import ConfigParser
+import mlflow
+import os
+from datasets import load_dataset
+import numpy as np
+def main():
+  model_config, mlflow_config = config()
+  mlflow.set_tracking_uri(mlflow_config['mlflow']['URL'])
+  random_int = np.random.randint(1000000)
+  mlflow.create_experiment(f"artifact_checks_seperate_test_{random_int}",
+                           artifact_location=mlflow_config['mlflow']['GCS_BUCKET'])
+  # base_artifact_uri = mlflow_config['mlflow']['GCS_BUCKET']
+  artifact_subpath = "model_path"
+  local_file_path = Path(__file__).parent / "pyproject.toml"
+  readme_path = Path(__file__).parent / "README.md"
+  
+  
+  dataset = load_dataset(model_config.dataset.name, split = model_config.dataset.split)
+  
+  with mlflow.start_run() as run:
+    mlflow.log_params(dict(model_config))
+    sample_examples = dataset.select(range(5)).to_dict()
+    sample_file = "sample_dataset.txt"
+    with open(sample_file, "w") as f:
+        f.write(str(sample_examples))
+    mlflow.log_artifact(sample_file, artifact_path="dataset_samples")
+    
+    trainer_model = get_trainer_model(
+      model_name = model_config.model_name,
+      chat_template = model_config.chat_template,
+      dataset = dataset,
+      peft_adapters = model_config.peft_adapters,
+      sft_configs = model_config.sft_configs,
+      peft_configs = model_config.peft_configs,
+      max_seq_length = model_config.max_seq_length,
+      dtype = model_config.dtype,
+      load_in_4bit = model_config.load_in_4bit,
+    )
+    trainer_stats = trainer.train()
+    model = trainer.model
+    if trainer.state.log_history and "loss" in trainer.state.log_history[-1]:
+        final_loss = trainer.state.log_history[-1]["loss"]
+        mlflow.log_metric("final_train_loss", final_loss)
+    model_save_path = "saved_model"
+    model.save_pretrained_gguf(model_save_path,tokenizer, quantization_method ="q5_k_m")  # Local saving
+    os.makedirs(model_save_path, exist_ok=True)  
+    mlflow.log_artifact("./saved_model/unsloth.Q5_K_M.gguf", artifact_path="model_path/unsloth.Q5_K_M.gguf")  
+    print(f"MLflow run {run.info.run_id} completed.")
+
+def config():
+  config_path = Path(__file__).parent / "configs" / "finetuning.yaml"
+  model_config = OmegaConf.load(config_path)
+  OmegaConf.resolve(model_config)
+  
+  mlflow_config_path = Path(__file__).parent.parent.parent / "scripts/config.ini"
+  mlflow_config = ConfigParser()
+  mlflow_config.read(mlflow_config_path)
+  os.environ['MLFLOW_TRACKING_USERNAME'] = mlflow_config['mlflow']['TRACKING_USERNAME']
+  os.environ['MLFLOW_TRACKING_PASSWORD'] = mlflow_config['mlflow']['TRACKING_PASSWORD']
+  return model_config, mlflow_config
+
+if __name__ == "__main__":
+  main()
---
+++ packages/ml/src/ml/finetuning/unsloth.py
@@ -0,0 +1,80 @@
+from unsloth import FastLanguageModel
+from unsloth.chat_templates import get_chat_template,standardize_sharegpt
+import torch
+from trl import SFTTrainer
+from transformers import TrainingArguments, DataCollatorForSeq2Seq
+from unsloth import is_bfloat16_supported
+
+# max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+# dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+# load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+# model_name ="unsloth/Llama-3.2-3B-Instruct" 
+# chat_template = "llama-3.1"
+
+# sft_configs = dict(
+#     max_seq_length = max_seq_length,
+#     dataset_text_field = "text",
+#     dataset_num_proc = 2,
+#     packing = False, # Can make training 5x faster for short sequences.
+# )
+# peft_adapters = dict(
+#     r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+#     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+#                       "gate_proj", "up_proj", "down_proj",],
+#     lora_alpha = 16,
+#     lora_dropout = 0, # Supports any, but = 0 is optimized
+#     bias = "none",    # Supports any, but = "none" is optimized
+#     # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+#     use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+#     random_state = 3407,
+#     use_rslora = False,  # We support rank stabilized LoRA
+#     loftq_config = None, # And LoftQ
+# )
+
+# dataset = load_dataset("mlabonne/FineTome-100k", split = "train[:100]")
+
+
+def formatting_prompts_func(examples, tokenizer):
+    convos = examples["conversations"]
+    texts = [tokenizer.apply_chat_template(convo, tokenize = False, add_generation_prompt = False) for convo in convos]
+    return { "text" : texts, }
+
+def get_trainer_model(model_name, chat_template, dataset, peft_adapters, sft_configs, peft_configs , max_seq_length, dtype, load_in_4bit):
+    model, tokenizer = FastLanguageModel.from_pretrained(
+                                          model_name = model_name , # or choose "unsloth/Llama-3.2-1B-Instruct"
+                                          max_seq_length = max_seq_length,
+                                          dtype = dtype,
+                                          load_in_4bit = load_in_4bit,
+                                          # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+                                        )
+    model = FastLanguageModel.get_peft_model(
+        model,
+        **peft_adapters,
+    )
+
+    tokenizer = get_chat_template(
+        tokenizer,
+        chat_template = chat_template,
+    )
+
+    dataset = standardize_sharegpt(dataset)
+    dataset = dataset.map(formatting_prompts_func, batched = True,)
+
+    trainer = SFTTrainer(
+            model = model,
+            tokenizer = tokenizer,
+            train_dataset = dataset,
+            data_collator = DataCollatorForSeq2Seq(tokenizer = tokenizer),
+            **sft_configs,
+            args = TrainingArguments(
+              **peft_configs
+            ),
+        )
+
+    trainer = train_on_responses_only(
+        trainer,
+        instruction_part = "<|start_header_id|>user<|end_header_id|>\n\n",
+        response_part = "<|start_header_id|>assistant<|end_header_id|>\n\n",
+        )
+    return trainer
