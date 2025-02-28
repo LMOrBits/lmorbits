98d168fa3e5d4334acc5716eb0b4412de28e843e
parsa <parsa.miraghaie@gmail.com>
2025-02-28 12:42:38 +0200

    removed the app package to use it from its repo

--- packages/app/pyapp/src/pyapp/workflows/chain.py
+++
@@ -1,2 +0,0 @@
-def pychain():
-  return "pychain"
--- packages/app/pyapp/usecases/chatbot/__main__.py
+++
@@ -1,8 +0,0 @@
-from pyapp.workflows.chain import pychain
-def main():
-    print(pychain())
-    print("Hello from test1!")
-
-
-if __name__ == "__main__":
-    main()
